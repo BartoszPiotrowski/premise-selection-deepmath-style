a0a59e6a9026798f5e00e5bdabf3c01d39dfc702
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2017-10-18 15:26:34 +0200

    Cleaning.

--- tools/saving_loading.py
+++ tools/saving_loading.py
@@ -52,26 +52,3 @@ def printll(ll):
     for l in ll:
         print(" ".join(map(str, l)))
 
-# Something is wrong with the function below -- messed header; don't use it for
-# now
-def GridCV_scores_to_csv(gs_clf, export_file):
-    with open(export_file, 'w') as outfile:
-        csvwriter = csv.writer(outfile, delimiter=',')
-
-    # Create the header using the parameter names
-        header = ["mean","std"]
-        param_names = [param for param in gs_clf.param_grid]
-        header.extend(param_names)
-
-        csvwriter.writerow(header)
-
-        for config in gs_clf.cv_results_:
-            # Get mean and standard deviation
-            mean = config[1]
-            std = np.std(config[2])
-            row = [mean,std]
-
-            # Get the list of parameter settings and add to row
-            params = [str(p) for p in config[0].values()]
-            row.extend(params)
-            csvwriter.writerow(row)
