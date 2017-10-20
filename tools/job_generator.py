17f63364c6b5b6507fb65993a3d82d36a014a9e1
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2017-10-20 14:48:42 +0200

    Dictionary is unordered, especially in python 3.5

--- tools/job_generator.py
+++ tools/job_generator.py
@@ -13,17 +13,17 @@ def generate_jobs(template, grid, output_directory):
         i = int(f)
         return i if i == f else f
 
-    g = {}
+    params, numbers = [], []
     for l in list_of_lines:
-        param, numbers_str = l.split(":")
-        numbers = [int_float(i) for i in numbers_str.split(", ")]
-        g[param] = numbers
-    comb = list(product(*list(g.values())))
+        p, n = l.split(":")
+        ns = [int_float(i) for i in n.split(", ")]
+        params.append(p); numbers.append(ns)
+    comb = list(product(*numbers))
     template = Template(open(template).read())
     for i in comb:
-        d = dict(zip(g.keys(), i))
+        d = dict(zip(params, i))
         job = template.substitute(d)
         job_file_name = os.path.join(output_directory,
-                        "_".join(map(str, list(d.values())))+".job.py")
+                        "_".join(map(str, i))+".job.py")
         with open(job_file_name, "w") as txt:
             txt.write(job)
